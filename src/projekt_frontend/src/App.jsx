83b8e050d69cb76e9a803fbd15402361a5c2e1c6
Faker389 <michal200614@gmail.com>
2024-06-24 19:58:55 +0200

    first commit

--- src/projekt_frontend/src/App.jsx
+++ src/projekt_frontend/src/App.jsx
@@ -1,29 +1,8 @@
-import { useState } from 'react';
-import { projekt_backend } from 'declarations/projekt_backend';
-
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    projekt_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <h1 className="">huj</h1>
     </main>
   );
 }
